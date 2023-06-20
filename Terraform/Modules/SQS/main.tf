9f22ce0479d33d8efc4c576c55ea75e4b4718708
HuntHub <105337456+HuntHub@users.noreply.github.com>
2023-06-20 13:05:28 -0400

    Deleted to conform with case-sensitivity

--- Terraform/Modules/SQS/main.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_sqs_queue" "order_events" {
-  name = "order-events-queue"
-}
--- Terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "Create_Order_Endpoint" {
-  value = module.apigateway.api_invoke_url
-  description = "URL for the API Gateway"
-}
-
-output "Database_Table_Name" {
-  value = module.dynamodb.dynamo_table_name
-  description = "Name of the DynamoDB table"
-}
