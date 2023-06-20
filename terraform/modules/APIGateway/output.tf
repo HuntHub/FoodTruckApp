61e5cfd380ef0641b5ee23312d8282d15e1965e7
HuntHub <105337456+HuntHub@users.noreply.github.com>
2023-06-20 13:04:23 -0400

    Deleted to conform with case-sensitivity

--- terraform/modules/APIGateway/output.tf
+++
@@ -1,4 +0,0 @@
-output "api_invoke_url" {
-  description = "The URL to invoke the API Gateway"
-  value       = "${aws_api_gateway_deployment.deployment.invoke_url}/${aws_api_gateway_resource.resource.path}"
-}
